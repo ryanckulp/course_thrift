83dd3eb5ab771fa7f656c0cc4684042d57e1c814
Ryan Kulp <ryanckulp@gmail.com>
2023-07-26 18:38:29 -0400

    build slugs and other validations for listings/courses

--- app/models/course.rb
+++ app/models/course.rb
@@ -5,12 +5,45 @@ class Course < ApplicationRecord
   has_one_attached :featured_image
 
   validates_presence_of :url
+  validates_uniqueness_of :url
 
   scope :published, -> { where(published: true) }
+  scope :purchaseable, -> { published } # used on /courses
+
+  before_validation do
+    sanitize_url
+  end
 
   after_create_commit :import
 
+  def sanitize_url
+    self.url = self.url.split('?')[0].split('#')[0].chomp('/')
+  end
+
   def import
     CourseScraper.new(self).call
+  end
+
+  def lowest_price
+    listings.purchaseable.lowest_priced.first.price rescue nil
+  end
+
+  def next_lowest_price
+    listings.purchaseable.lowest_priced.second.price rescue nil
+  end
+
+  def extra_listings_available?
+    listings.purchaseable.count > 1
+  end
+
+  # TODO: add 'coupon / discount / etc suffixes'
+  def slugify!
+    parameterized = title.parameterize
+
+    if Course.where(slug: parameterized).exists?
+      parameterized = "#{parameterized}-#{SecureRandom.hex(3)}"
+    end
+
+    update(slug: parameterized)
   end
 end
--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,4 +1,28 @@
 class Listing < ApplicationRecord
   belongs_to :course
   belongs_to :user
+
+  scope :with_payment_url, -> { where.not(payment_url: nil) }
+  scope :lowest_priced, -> { order(discount_percent: :desc) }
+  scope :unsold, -> { where(sold_at: nil) }
+  scope :purchaseable, -> { unsold.with_payment_url }
+
+  before_create :generate_code
+  after_create_commit :create_payment_url
+
+  def generate_code
+    self.code = SecureRandom.hex(4)
+  end
+
+  def create_payment_url
+    PaymentLinker.new(self).call
+  end
+
+  def price
+    course.original_price.to_f - (course.original_price.to_f * (discount_percent.to_f / 100))
+  end
+
+  def mark_sold!
+    update(sold_at: DateTime.now)
+  end
 end
