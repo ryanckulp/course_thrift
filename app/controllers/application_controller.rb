35faf32aff93fbe4339f49b1745766354a682ac5
Ryan Kulp <ryanckulp@gmail.com>
2023-07-26 18:38:02 -0400

    removed SaaS subscribe redirects

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   def after_sign_in_path_for(resource)
-    resource.paying_customer? ? dashboard_index_path : subscribe_index_path # point these wherever you want
+    courses_path
 	end
 
   def maybe_skip_onboarding
