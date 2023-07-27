1dff0f12940fcb9764be3119cc1c717db73628d7
sljmn <shot@hey.com>
2023-07-28 00:36:41 +0200

    added slugify here, it wasnt called anywhere

--- app/services/course_scraper.rb
+++ app/services/course_scraper.rb
@@ -1,66 +1,70 @@
+require 'nokogiri'
+require 'open-uri'
+require 'json'
+
 class CourseScraper
-  attr_accessor :course, :doc
-  require 'nokogiri'
-  require 'open-uri'
-  require 'json'
+  attr_accessor :course, :site_content
 
   def initialize(course)
     @course = course
     # PODIA.com only courses for this MVP proof of concept
+
   end
 
   def call
-    save_course
+    scrape_course_data
+    save_course_metadata
   end
 
   handle_asynchronously :call
 
   private
 
-
-  def doc
-    @doc ||= Nokogiri::HTML(URI.open(course.url)) # make it memoized so we don't have to keep hitting the URL
+  def site_content
+    @site_content ||= Nokogiri::HTML(URI.open(course.url))
   end
 
-  def save_course
+  def scrape_course_data
     course.update({
       title: title,
       description: description,
       original_price: price,
     })
-
-    course.slugify!
-    course.featured_image.attach(io: OpenURI.open_uri(image), filename: "course_#{course.id}.jpg")
+    attach_featured_image
+    course.slugify! # calling it here bc otherwise we are to early when calling from the course modal.. todo: fix this
   end
 
+  def course_data
+    site_content.at('[data-react-class="creator_ui/section_adapters/ProductBanner"]')
+  end
 
   def title
-    title = doc.at_css('meta[property="og:title"]')['content']
+    site_content.at_css('meta[property="og:title"]')['content']
   end
 
   def description
-    description_meta = doc.at_css('meta[name="description"]')
-    description_content = description_meta['content']
+    description_meta = site_content.at_css('meta[name="description"]')
+    description_meta['content']
   end
 
   def price
-    element = doc.at('[data-react-class="creator_ui/section_adapters/ProductBanner"]')
-
-    return 0 if element.nil?
-
-    react_props = JSON.parse(element['data-react-props'])
-    price_with_dollar_sign = react_props['prices']['full']
-    price = price_with_dollar_sign.gsub(/[^\d\.]/, '').to_f
-
+    return 0 if course_data.nil?
+    price_with_dollar_sign.gsub(/[^\d\.]/, '').to_f
   end
 
-  def image
-    element = doc.at('[data-react-class="creator_ui/section_adapters/ProductBanner"]')
-
-    react_props = JSON.parse(element['data-react-props'])
-    images = react_props["images"]
-
-    images['primary']
+  def price_with_dollar_sign
+    react_props['prices']['full']
   end
 
+  def react_props
+    JSON.parse(course_data['data-react-props'])
+  end
+
+  def fetch_image
+    react_props["images"]['primary']
+  end
+
+  def attach_featured_image
+    course.featured_image.attach(io: OpenURI.open_uri(fetch_image), filename: "course_#{course.id}.jpg")
+  end
 end
